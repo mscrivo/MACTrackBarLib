ec76306871d6afa69c81c89e551753b924f96adb
Michael Scrivo <mscrivo@gmail.com>
2019-03-20 19:56:34 -0400

    Migrate to new csproj format

--- MACTrackBarLib/AssemblyInfo.cs
+++ MACTrackBarLib/AssemblyInfo.cs
@@ -1,33 +1,6 @@
-using System.Resources;
+﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-//
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]		
-
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.0.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -58,5 +31,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
 [assembly: ComVisible(false)]
-[assembly: NeutralResourcesLanguage("en-US")]
 
